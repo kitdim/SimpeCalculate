045882746dab86c16a91486cb0e2f1b6c3801bd0
kitdim <kitaykin9808@gmail.com>
2023-12-02 12:37:45 +0300

    add test

--- src/test/java/org/kit/task/CalculateTest.java
+++ src/test/java/org/kit/task/CalculateTest.java
@@ -1,19 +1,35 @@
 package org.kit.task;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.kit.task.parser.Parser;
+import org.kit.task.service.Calculate;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculateTest {
+    private static List<String> data;
+
+    @BeforeEach
+    void init() {
+        data = List.of("1+2-3", "-3+2", "2*(3)", "2-(3)");
+    }
+
     @Test
     void wasGetResult() {
-        var actual = Parser.evaluateExpression("1+2-3");
+        var item = Parser.evaluateExpression(data.get(0));
+        var actual = Calculate.calc(item);
         assertThat(actual).isEqualTo(0);
-        actual = Parser.evaluateExpression("-3+2");
+        item = Parser.evaluateExpression(data.get(1));
+        actual = Calculate.calc(item);
         assertThat(actual).isEqualTo(-1);
-        actual = Parser.evaluateExpression("2*(-3)");
-        assertThat(actual).isEqualTo(-6);
-        actual = Parser.evaluateExpression("2-(-3)");
-        assertThat(actual).isEqualTo(5);
+        item = Parser.evaluateExpression(data.get(2));
+        actual = Calculate.calc(item);
+        assertThat(actual).isEqualTo(6);
+        item = Parser.evaluateExpression(data.get(3));
+        actual = Calculate.calc(item);
+        assertThat(actual).isEqualTo(-1);
     }
 }
